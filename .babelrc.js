288c53b370ef7f2838f1b865d36a9794fb8e6275
citycide <thecitycide@gmail.com>
2018-06-16 22:45:49 -0500

    chore(package): update dependencies

--- .babelrc.js
+++ .babelrc.js
@@ -2,6 +2,8 @@ module.exports = {
   presets: [['@babel/env', {
     targets: { node: 6 },
     loose: true
-  }], '@babel/stage-1'],
+  }], ['@babel/stage-1', {
+    decoratorsLegacy: true
+  }]],
   plugins: ['babel-plugin-macros']
 }
